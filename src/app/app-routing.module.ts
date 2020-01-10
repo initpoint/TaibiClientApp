f20bf947a534ba7fc2c5763eeeb5e8f12c30d979
Mohamed Elsayed <Mohamed.Elsayed.SY@gmail.com>
2020-01-11 01:17:19 +0200

    Adding directors and other pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,8 @@ const routes: Routes = [
       // pages route
       { path: 'dashbord', loadChildren: () => import('./pages/dashbord/dashbord.module').then(m => m.DashbordModule), canActivate: [AuthGuard] },
       { path: 'universities', loadChildren: () => import('./pages/universities/universities.module').then(m => m.UniversitiesModule), canActivate: [AuthGuard] },
+      { path: 'directories', loadChildren: () => import('./pages/directories/directories.module').then(m => m.DirectoriesModule), canActivate: [AuthGuard] },
+      { path: 'vacancies', loadChildren: () => import('./pages/vacancies/vacancies.module').then(m => m.VacanciesModule), canActivate: [AuthGuard] },
       { path: 'events', loadChildren: () => import('./pages/events/events.module').then(m => m.EventsModule), canActivate: [AuthGuard] },
       { path: 'my-profile', loadChildren: () => import('./pages/user-managment/my-profile/my-profile.module').then(m => m.MyProfileModule), canActivate: [AuthGuard] },
       { path: 'super-admins', loadChildren: () => import('./pages/super-admins/super-admins.module').then(m => m.SuperAdminsModule), canActivate: [AuthGuard] },
